7828d598683b7e5fc118851b09b53d8f5ccb00e2
TORY-SKY <ezevictor559@gmail.com>
2025-09-29 06:49:24 +0100

    run time errors fixed

--- korna_app/src/App.tsx
+++ korna_app/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+
 import Navbar from "./Navbar/Navbar.tsx"
 import BlogPost from "./Components/BlogPost.tsx"
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
